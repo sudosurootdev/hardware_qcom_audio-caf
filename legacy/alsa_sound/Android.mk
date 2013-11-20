986465046f21eb999e552556dd22fb5e435a3b54
nuclearmistake <nuclearmistake@gmail.com>
2013-11-19 21:14:52 -0500

    legacy/alsa_sound: fix aliasing Werror
    Change-Id: Iae1a25c17565e1788264faf379c45365e6b032fa

--- legacy/alsa_sound/Android.mk
+++ legacy/alsa_sound/Android.mk
@@ -138,7 +138,9 @@ include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_CFLAGS += $(common_cflags)
+LOCAL_CFLAGS += $(common_cflags) \
+  -Wno-error=strict-aliasing \
+  -fno-strict-aliasing
 
 LOCAL_SRC_FILES := \
   AudioHardwareALSA.cpp         \
