8db61179c6aecba3073587a91cd22d1d1d31089f
DHOMD <DHO@dho.im>
2014-02-26 22:06:20 -0500

    Revert "offload_visualizer: "violate" aliasing violations" || Lazy Fix || Fixes several builds
    This reverts commit 737f7a64870169e8fca2189a2cf003a15ef184f5.
    
    Change-Id: I05dee76b11cc5a87c6dbf6633235770c48d67bbd

--- visualizer/Android.mk
+++ visualizer/Android.mk
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
 	offload_visualizer.c
 
-LOCAL_CFLAGS+= -O2 -fvisibility=hidden
+LOCAL_CFLAGS+= -O2 -fvisibility=hidden -fno-strict-aliasing -Wno-error=strict-aliasing 
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
