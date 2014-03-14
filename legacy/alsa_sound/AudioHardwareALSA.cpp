3f704fd0be0d021439a0d32d25d6dbc17993c17d
Pavan Chikkala <pavanc@codeaurora.org>
2014-03-14 18:11:49 +0530

    audio: Update call state from RIL during silent mode
     -In silent mode, after call is rejected from Far end, RIL
      sets call state to active to play the network message and
      sets the call_state to inactive later without informing
      the telephony framework.So, setMode() is not called from
      framework.
     -Currently,we are updating call state only if there is any
      active call or call state being set from RIL is "active".
      Fix is to update the call state when inactive state is
      received from RIL for the same VSID
    
    CRs-Fixed: 631062
    
    Change-Id: I937341c48fd4950aab2e9e689712b47976b3c28d

--- legacy/alsa_sound/AudioHardwareALSA.cpp
+++ legacy/alsa_sound/AudioHardwareALSA.cpp
@@ -837,7 +837,7 @@ status_t AudioHardwareALSA::setParameters(const String8& keyValuePairs)
         if (param.getInt(key, tmp) == NO_ERROR) {
             call_state = (enum call_state)tmp;
             param.remove(key);
-            if (isAnyCallActive() || (call_state == CALL_ACTIVE)) {
+            if (isAnyCallActive() || (call_state == CALL_ACTIVE) ||((call_state == CALL_INACTIVE) && mVSID == vsid)) {
                 mVSID = vsid;
                 mCallState = call_state;
                 ALOGD("%s() vsid:%x, callstate:%x", __func__, mVSID, call_state);
@@ -2500,7 +2500,7 @@ bool AudioHardwareALSA::routeCall(int device, int newMode, uint32_t vsid)
         break;
 
     case CALL_ACTIVE:
-        if (*curCallState == CALL_INACTIVE) {
+        if (*curCallState == CALL_INACTIVE && newMode == AUDIO_MODE_IN_CALL) {
             ALOGV("%s(): Start call for vsid:%x ",__func__, vsid);
 
             status = enableVoiceCall(newMode, device, vsid);
